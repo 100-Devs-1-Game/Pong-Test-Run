e246af6b63b1b352c7c8957cddbc84b3e55e6108
thaeta <65732167+Thaeta-Of-Diamonds@users.noreply.github.com>
2025-07-18 16:44:55 -0400

    Revert "Add physics module"

--- src/global/event_bus.gd
+++ src/global/event_bus.gd
@@ -3,12 +3,3 @@ extends Node
 
 # the place for all global signals
 # add individual regions for modules
-
-#region physics
-signal updated_ball_position(pos: Vector2)
-signal updated_ball_velocity(vel: Vector2)
-signal updated_player_paddle_position(pos: Vector2)
-signal updated_opponent_paddle_position(pos: Vector2)
-signal updated_player_paddle_rotation(rot: float)
-signal updated_opponent_paddle_rotation(rot: float)
-#endregion
--- src/modules/physics/paddle.gd
+++
@@ -1,2 +0,0 @@
-class_name Paddle
-extends Node2D
