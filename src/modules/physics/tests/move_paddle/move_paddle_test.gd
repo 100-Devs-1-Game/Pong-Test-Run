c6c4cb754fc8e6c4dd6cdf7112d6cd8c7b35a08e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-19 00:56:47 +0200

    Update move_paddle_test.gd

--- src/modules/physics/tests/move_paddle/move_paddle_test.gd
+++ src/modules/physics/tests/move_paddle/move_paddle_test.gd
@@ -1,8 +1,3 @@
 extends Node2D
 
 @onready var paddle: Paddle = $Paddle
-
-
-func _process(delta: float) -> void:
-    var dir: float = Input.get_axis("ui_up", "ui_down")
-    paddle.current_direction = dir
