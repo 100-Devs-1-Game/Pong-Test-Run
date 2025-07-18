29d346d8d27baa5610994488cffba1de67e13969
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-18 23:53:03 +0200

    Formatting

--- src/modules/physics/paddle.gd
+++ src/modules/physics/paddle.gd
@@ -8,19 +8,19 @@ var current_direction: float
 
 
 func _ready():
-    # TODO wait for signal name
-    #EventBus.player_move_input.connect(on_move)
-    #EventBus.player_rotation_input.connect(on_rotate)
-    pass
+	# TODO wait for signal name
+	#EventBus.player_move_input.connect(on_move)
+	#EventBus.player_rotation_input.connect(on_rotate)
+	pass
 
 
 func on_move(direction: float):
-    current_direction = direction
+	current_direction = direction
 
 
 func on_look_at(towards: Vector2):
-    look_at(towards)
+	look_at(towards)
 
 
 func _physics_process(delta: float) -> void:
-    position.y += current_direction * max_move_speed * delta
+	position.y += current_direction * max_move_speed * delta
