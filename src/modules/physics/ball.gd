5280a392bacce65a88d9e5c3a890d5954e6592e9
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-18 22:39:54 +0200

    Add core functionality

--- src/modules/physics/ball.gd
+++ src/modules/physics/ball.gd
@@ -1,2 +1,11 @@
 class_name Ball
 extends Node2D
+
+var velocity: Vector2
+
+
+func _physics_process(delta: float) -> void:
+    position += velocity * delta
+
+    EventBus.updated_ball_velocity.emit(velocity)
+    EventBus.updated_ball_position.emit(position)
