ee75d083ea55b7b9fc85efce0673990804b83656
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-07-18 22:26:35 +0200

    Update event_bus.gd

--- src/global/event_bus.gd
+++ src/global/event_bus.gd
@@ -3,3 +3,12 @@ extends Node
 
 # the place for all global signals
 # add individual regions for modules
+
+#region physics
+signal update_ball_position(pos: Vector2)
+signal update_ball_velocity(vel: Vector2)
+signal update_player_paddle_position(pos: Vector2)
+signal update_opponent_paddle_position(pos: Vector2)
+signal update_player_paddle_rotation(rot: float)
+signal update_opponent_paddle_rotation(rot: float)
+#endregion
