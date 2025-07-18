1b3e785a014abbd07bda5b40605a567dcf0b3c4f
Noxalas-Desktop <NatanielWlo@gmail.com>
2025-07-19 01:16:16 +0200

    Renamed methods and input actions for clarity

--- src/modules/paddle_controller/paddle_controller.gd
+++ src/modules/paddle_controller/paddle_controller.gd
@@ -2,13 +2,13 @@ class_name PaddleController
 extends Node
 
 
-func get_movement_direction() -> float:
+func get_vertical_input() -> float:
     return 0.0
 
 
-func get_tilt_direction() -> float:
+func get_tilt_input() -> float:
     return 0.0
 
 
-func get_look_direction(_from_position: Vector2) -> Vector2:
+func get_look_vector(_from_position: Vector2) -> Vector2:
     return Vector2.ZERO
